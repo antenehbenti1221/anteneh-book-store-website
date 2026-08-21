c714ec8333879232a562d75d3e9b94496914fc1a
antenehbenti1221 <antenehbenti1221@gmail.com>
2026-08-21 18:29:11 +0300

    Connect website to existing Anteneh Book Store Telegram bot

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.STORE_CONFIG={
   SUPABASE_URL:"https://eyhrzdmdnpuimzfqyith.supabase.co",
   SUPABASE_PUBLISHABLE_KEY:"sb_publishable_-mrlZiyICBjYjGWcdzx9-g_b07T45oS",
-  TELEGRAM_USERNAME:"PASTE_YOUR_TELEGRAM_USERNAME_HERE"
+  TELEGRAM_USERNAME:"AntenehBookStoreBot"
 };
