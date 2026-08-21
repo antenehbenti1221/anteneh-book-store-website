e4d7e6be3bbef3948f78c3a3422d420a109cc8dd
antenehbenti1221 <antenehbenti1221@gmail.com>
2026-08-21 18:40:40 +0300

    Make website independent from Telegram

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 window.STORE_CONFIG={
   SUPABASE_URL:"https://eyhrzdmdnpuimzfqyith.supabase.co",
-  SUPABASE_PUBLISHABLE_KEY:"sb_publishable_-mrlZiyICBjYjGWcdzx9-g_b07T45oS",
-  TELEGRAM_USERNAME:"AntenehBookStoreBot"
+  SUPABASE_PUBLISHABLE_KEY:"sb_publishable_-mrlZiyICBjYjGWcdzx9-g_b07T45oS"
 };
